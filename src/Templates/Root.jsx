c47789677f774d265e0c80b1c5f5c311cc14982f
Abdel Raheem <AbdelRaheemSakr@gmail.com>
2023-10-28 19:31:33 +0200

    add palestine flag #free_palestine

--- src/Templates/Root.jsx
+++ src/Templates/Root.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import PalestineFlag from "../Atoms/PalestineFlag";
 
 export default function Root() {
   return (
     <>
       <Outlet />
+      <div className="sticky w-full bottom-0 left-0">
+        <PalestineFlag />
+      </div>
     </>
   );
 }
